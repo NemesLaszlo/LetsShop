f59a179cf781fcd5612e1d049487c04c0bdd373c
NemesLaszlo <wow.laszlo@gmail.com>
2021-09-20 19:14:21 +0200

    frontend Pagination implementation

--- LetsShop-Frontend/src/app/shop/shop.component.ts
+++ LetsShop-Frontend/src/app/shop/shop.component.ts
@@ -87,4 +87,11 @@ export class ShopComponent implements OnInit {
     this.getProducts();
   }
 
+  onPageChanged(event: any) {
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
 }
