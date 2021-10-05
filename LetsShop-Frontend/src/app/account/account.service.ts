2f9e238f33e1026948f658d779f67a594b6f428e
NemesLaszlo <wow.laszlo@gmail.com>
2021-10-05 17:38:42 +0200

    frontend account services

--- LetsShop-Frontend/src/app/account/account.service.ts
+++ LetsShop-Frontend/src/app/account/account.service.ts
@@ -1,9 +1,70 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { of, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../shared/_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new ReplaySubject<IUser>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(values: any) {
+    return this.http.post(this.baseUrl + 'account/login', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(values: any) {
+    return this.http.post(this.baseUrl + 'account/register', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  loadCurrentUser(token: string) {
+    if (token == null) {
+      this.currentUserSource.next(null);
+      return of(null);
+    }
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+
+    return this.http.get(this.baseUrl + 'account', {headers}).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  checkEmailExists(email: string) {
+    return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
+  }
+  
 }
