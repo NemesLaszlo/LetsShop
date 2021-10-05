40d878cd946cfd1e66a1e9353d9fdcb8e53a32b6
NemesLaszlo <wow.laszlo@gmail.com>
2021-10-05 23:10:16 +0200

    frontend register component

--- LetsShop-Frontend/src/app/shared/components/text-input/text-input.component.ts
+++ LetsShop-Frontend/src/app/shared/components/text-input/text-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, Self, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
